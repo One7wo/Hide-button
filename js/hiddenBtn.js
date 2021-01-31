6957a5e856eee214b4b773ee01f0f8952ee56f91
Максим Гончар <71597110+One7wo@users.noreply.github.com>
2021-01-31 21:15:04 +0300

    conept end

--- js/hiddenBtn.js
+++ js/hiddenBtn.js
@@ -4,8 +4,10 @@ const onScrollStopped = (domElem, callback, delay = 200) => {
         transition: all 0.2s ease;
         bottom: -55px;`
 
-        if(window.pageYOffset > 96){
-        document.getElementsByClassName('gear')[0].style.cssText = `
+        if (window.pageYOffset > 96
+            && window.pageYOffset < document.getElementsByClassName('point')[0].offsetTop - window.innerHeight
+            && window.pageYOffset > document.getElementsByClassName('point')[0].offsetTop + 80) {
+            document.getElementsByClassName('gear')[0].style.cssText = `
                 
                 position: fixed;
                 animation-name: rotation-gear-scroll;
@@ -13,62 +15,88 @@ const onScrollStopped = (domElem, callback, delay = 200) => {
                 animation-iteration-count: infinite;
                 animation-timing-function: linear;
         `}
-   
+        if (document.getElementsByClassName('point')[0].offsetTop > window.pageYOffset
+            && document.getElementsByClassName('point')[0].offsetTop < window.pageYOffset + window.innerHeight) {
+            setTimeout(() => {
+                document.getElementsByClassName('gear')[0].style.cssText = `
+            top: -95px;
+
+            transition: all 0.2s ease;
+            
+            `
+            }, 200)
+        }
+        else {
+
+            setTimeout(() => {
+                document.getElementsByClassName('gear')[0].style.cssText = `
+            top: 95px;
+
+            transition: all 0.2s ease;
+            
+            `
+            }, 200)
+        }
+
         clearTimeout(callback.delay);
         callback.delay = setTimeout(callback, delay);
     });
 }
-onScrollStopped(window, () =>{
+
+onScrollStopped(window, () => {
     document.getElementsByClassName('f-btn')[0].style.cssText = `
         transition: all 0.2s ease;
         bottom: 0px;
     `
-    document.getElementsByClassName('gear')[0].style.cssText = 
-    `
-    position: absolute;
-    animation-name: move-gear;
-    animation-duration: 0.7s;
-    animation-iteration-count: 1;
-    animation-timing-function: ease-out;
-    `
+    if (document.getElementsByClassName('point')[0].offsetTop > window.pageYOffset
+        && document.getElementsByClassName('point')[0].offsetTop < window.pageYOffset + window.innerHeight - 55) {
 
-    let moveGearAnimation = document.createElement('style');
-    moveGearAnimation.type = 'text/css';
-    let moveGear = document.createTextNode(
+    }
+    else {
+        document.getElementsByClassName('gear')[0].style.cssText =
+            `
+            position: absolute;
+            animation-name: move-gear;
+            animation-duration: 0.7s;
+            animation-iteration-count: 1;
+            animation-timing-function: easeOutQuad;
         `
+
+        let moveGearAnimation = document.createElement('style');
+        moveGearAnimation.type = 'text/css';
+        let moveGear = document.createTextNode(
+            `
         @keyframes move-gear {
             from{
-                top: ${window.pageYOffset+95}px;
+                top: ${window.pageYOffset + 95}px;
                 transform:rotate(720deg);
             }
             50%{
-                top: ${window.pageYOffset+130}px;
+                top: ${window.pageYOffset + 160}px;
                 transform:rotate(1080deg);
             }
             to{
                 
-                top: ${window.pageYOffset+95}px;
+                top: ${window.pageYOffset + 95}px;
                 transform:rotate(360deg);
             }
         }
         `
-        
-    )
-    moveGearAnimation.appendChild(moveGear);
-    document.getElementsByClassName('gear')[0].appendChild(moveGearAnimation);
-    
-    
-    
-    setTimeout(() =>{
-    document.getElementsByClassName('gear')[0].style.cssText = 
+
+        )
+        moveGearAnimation.appendChild(moveGear);
+        document.getElementsByClassName('gear')[0].appendChild(moveGearAnimation);
+
+
+
+        setTimeout(() => {
+            document.getElementsByClassName('gear')[0].style.cssText =
+                `
+            animation-name: rotation-gear;
+            animation-duration: 5s;
+            animation-iteration-count: infinite;
+            animation-timing-function: linear;
     `
-        animation-name: rotation-gear;
-        animation-duration: 5s;
-        animation-iteration-count: infinite;
-        animation-timing-function: linear;
-    `
-    }, 700)
-    
-    
-      
+        }, 700)
+    }
 })
